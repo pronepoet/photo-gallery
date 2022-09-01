4128eca9d048ea67564ee89ebb309d41688ed767
pronepoet <ochiengphillip84@gmail.com>
2022-09-01 23:50:20 +0300

    Add additional info about animals

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ function getAnimal(){
     fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
     .then(response => response.json())
     .then(data => {
+        console.log(data)
     let image = document.querySelector('img.card-img')
-
+    let animalName = document.querySelector('h5#nameOfAni')
+    let animalDiet = document.querySelector('p#animalDiet')
+    let animalInfo = document.querySelector('p#animalInfo')
+    let btn = document.querySelector('button.btn')
     image.src = data.image_link
+    animalName.innerText = data.name
+    animalInfo.innerText = `Class of animal is ${data.animal_type}`
+    animalDiet.innerText = `Diet consists of ${data.diet}`
+   
+   
 })
 }
-let btn = document.querySelector('button.btn')
-btn = document.addEventListener('click', getAnimal)
-   
+
+btn = document.addEventListener('click', (event)=>{
+    event.preventDefault();
+ getAnimal();
+})
    
 
 
