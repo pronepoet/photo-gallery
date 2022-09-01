74261113f0f6cd99bedd52f4ce2744444eb7da56
pronepoet <ochiengphillip84@gmail.com>
2022-09-01 15:11:59 +0300

    put footer at the bottom of page

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ addEventListener('DOMContentLoaded', (event) => {
 function getAnimal(){
     fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
     .then(response => response.json())
-    .then(data => console.log(data.image_link))
+    .then(data => {
+    let image = document.querySelector('img.card-img')
+    
+    image.src = data.image_link
+})
 }
 
 getAnimal()
