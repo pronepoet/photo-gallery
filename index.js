6aef7cecd940aa1656ee219a8e15cafffb5c68df
pronepoet <ochiengphillip84@gmail.com>
2022-09-01 14:34:14 +0300

    create a function to fetch animal data

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 addEventListener('DOMContentLoaded', (event) => {
 
 
+function getAnimal(){
+    fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
+    .then(response => response.json())
+    .then(data => console.log(data.image_link))
+}
 
-
-
+getAnimal()
 
 
 });
