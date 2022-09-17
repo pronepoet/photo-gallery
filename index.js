f059403591f360b009e4896fe8372710d49e61d5
pronepoet <ochiengphillip84@gmail.com>
2022-09-17 04:51:41 +0300

    add more info to the animals displayed

--- index.js
+++ index.js
@@ -10,20 +10,16 @@ addEventListener('DOMContentLoaded', (event) => {
         .then(data => {
           let image1 = document.querySelector('img#image1') 
           let animalName1 = document.querySelector('h5.animal1')
-          let animalDiet1 = document.querySelector('p#animalDiet1')
-          let animalHabitat1 = document.querySelector('p#animalHabitat1')
+          let animalClass1 = document.querySelector('p.animalClass1')
           let didYou = document.querySelector('div.ddy')
           let h3 = document.createElement('h3')
           didYou.appendChild(h3)
-          h3.textContent = `Did you know the ${data[0].name}'s diet consists of ${data[0].diet} and is found in ${data[0].geo_range}`
-          console.log(h3)
+          h3.textContent = `Did you know the ${data[0].name} is a ${data[0].animal_type} who's diet consists of ${data[0].diet} and is found in ${data[0].geo_range}. They typically grow upto a lenght of ${data[0].length_max} feet and can live upto ${data[0].lifespan} years!`
+          console.log(data)
             image1.src = data[0].image_link
             animalName1.innerHTML = data[0].name
-          //   animalInfo.innerText = `Class of animal is ${data.animal_type}`
-          //   animalDiet.innerText = `Diet consists of ${data.diet}`
-          //  geo.innerText = `This type of ${data.animal_type} is found in ${data.geo_range}`
-          //  animalHabitat.innerText = `This ${data.animal_type}'s habitat is ${data.habitat}`
-           console.log(data[1])})
+            animalClass1.innerHTML = data[0].animal_type
+      })
       }
 
 
@@ -33,12 +29,14 @@ addEventListener('DOMContentLoaded', (event) => {
         .then(response => response.json())
         .then(data => {
           let animalName2 = document.querySelector('h5.animal2')
+          let animalClass2 = document.querySelector('p.animalClass2')
           animalName2.innerHTML = data[0].name
          let image2 = document.querySelector('img#image2')
          image2.src = data[0].image_link
+         animalClass2.innerHTML = data[0].animal_type
         })
       }
-
+    
       // gets the third batch
    
 function get3(){
@@ -48,6 +46,8 @@ function get3(){
     let animalName3 = document.querySelector('h5.animal3')
     animalName3.innerHTML = data[0].name
    let image3 = document.querySelector('img#image3')
+   let animalClass3 = document.querySelector('p.animalClass3')
+   animalClass3.innerHTML = data[0].animal_type
    image3.src = data[0].image_link
   })
 }
@@ -61,6 +61,8 @@ function get3(){
     let animalName4 = document.querySelector('h5.animal4')
     animalName4.innerHTML = data[0].name
  let image4 = document.querySelector('img#image4')
+ let animalClass4 = document.querySelector('p.animalClass4')
+ animalClass4.innerHTML = data[0].animal_type
  image4.src= data[0].image_link
   })
  }
@@ -73,6 +75,8 @@ function get3(){
     let animalName5 = document.querySelector('h5.animal5')
     animalName5.innerHTML = data[0].name
  let image5 = document.querySelector('img#image5')
+ let animalClass5 = document.querySelector('p.animalClass5')
+ animalClass5.innerHTML = data[0].animal_type
  image5.src= data[0].image_link
   })
  }
@@ -85,6 +89,8 @@ function get3(){
     let animalName6 = document.querySelector('h5.animal6')
     animalName6.innerHTML = data[0].name
  let image6 = document.querySelector('img#image6')
+ let animalClass6 = document.querySelector('p.animalClass6')
+ animalClass6.innerHTML = data[0].animal_type
  image6.src= data[0].image_link
   })
  }
@@ -97,6 +103,8 @@ function get3(){
     let animalName7 = document.querySelector('h5.animal7')
     animalName7.innerHTML = data[0].name
  let image7 = document.querySelector('img#image7')
+ let animalClass7 = document.querySelector('p.animalClass7')
+ animalClass7.innerHTML = data[0].animal_type
  image7.src= data[0].image_link
   })
  }
@@ -108,6 +116,8 @@ function get3(){
     let animalName8 = document.querySelector('h5.animal8')
     animalName8.innerHTML = data[0].name
  let image8 = document.querySelector('img#image8')
+ let animalClass8 = document.querySelector('p.animalClass8')
+ animalClass8.innerHTML = data[0].animal_type
  image8.src= data[0].image_link
   })
  }
